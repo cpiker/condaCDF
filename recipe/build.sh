5e1ff21959234723f674406f2133dbdaff958b1c
cpiker <cpiker@users.noreply.github.com>
2020-04-06 23:46:07 -0500

    Working around broken Anaconda NumPy 1.18 package

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,15 @@ sUname=$(uname)
 
 if [ "${sUname}" = "Linux" ]; then
 
+	echo "make OS=linux ENV=gnu CURSES=no all"
 	make OS=linux ENV=gnu CURSES=no all
+	echo "make install.lib.so INSTALLDIR=${PREFIX}"
 	make install.lib.so INSTALLDIR=${PREFIX}
 	
 elif [ "${sUname}" = "Darwin" ]; then
-
+	echo "make OS=macosx ENV=gnu CURSES=no all"
 	make OS=macosx ENV=gnu CURSES=no all
+	echo "make install.lib.dylib INSTALLDIR=${PREFIX} MACLIB=${PREFIX}/lib"
 	make install.lib.dylib INSTALLDIR=${PREFIX}
 	
 else
